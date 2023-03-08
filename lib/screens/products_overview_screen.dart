310ed826596331d64cb64650202b8ba0dbf7bba7
Junosoft <junosoftltd@gmail.com>
2023-03-08 17:18:53 +0200

    starting

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ProductsOverviewSCreen extends StatelessWidget {
+  const ProductsOverviewSCreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('products over view'),
+    );
+  }
+}
