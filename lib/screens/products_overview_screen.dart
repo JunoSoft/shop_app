5cc05d2f7fff9c345edc295813b071fd185b44fe
Junosoft <junosoftltd@gmail.com>
2023-03-09 09:47:23 +0200

    added product structure

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+
 
 class ProductsOverviewSCreen extends StatelessWidget {
   const ProductsOverviewSCreen({super.key});
