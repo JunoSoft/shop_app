90cec9ab2b045b3491067057760d995856d7b0bd
Junosoft <junosoftltd@gmail.com>
2023-03-09 10:48:07 +0200

    edited

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -6,11 +6,27 @@ class ProductItem extends StatelessWidget {
   final String id;
   final String title;
   final String imgUrl;
-  
-  const ProductItem({required this.id,required this.title,required this.imgUrl ,super.key});
+
+  const ProductItem(
+      {required this.id, required this.title, required this.imgUrl, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(child: Image.network(imgUrl));
+    return GridTile(
+      child: Image.network(
+        imgUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.favorite)),
+        trailing:
+            IconButton(onPressed: () {}, icon: const Icon(Icons.shopping_cart)),
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
   }
-} 
+}
