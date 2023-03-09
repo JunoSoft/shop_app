c40bdbe4cb2cbc1ea5282e1131fbd3aae892bf25
Junosoft <junosoftltd@gmail.com>
2023-03-09 10:16:32 +0200

    updated

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+} 
