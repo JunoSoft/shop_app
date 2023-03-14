45e34a28f6f6d5e82f2721fd38ec9c186ebd1f5d
Junosoft <junosoftltd@gmail.com>
2023-03-15 00:25:28 +0200

    updating products with http

--- lib/screens/edit_products.dart
+++ lib/screens/edit_products.dart
@@ -93,12 +93,8 @@ class _EditProductsScreenState extends State<EditProductsScreen> {
     }
     _form.currentState!.save();
     if (_editedProduct.id != null) {
-      Provider.of<Products>(context, listen: false)
+      await Provider.of<Products>(context, listen: false)
           .updateProduct(_editedProduct.id, _editedProduct);
-      setState(() {
-        _isLoading = false;
-      });
-      Navigator.of(context).pop();
     } else {
       try {
         await Provider.of<Products>(context, listen: false)
@@ -117,13 +113,18 @@ class _EditProductsScreenState extends State<EditProductsScreen> {
                         child: const Text('Okay')),
                   ],
                 ));
-      } finally {
-        setState(() {
-          _isLoading = false;
-        });
-        Navigator.of(context).pop();
       }
+      // finally {
+      //   setState(() {
+      //     _isLoading = false;
+      //   });
+      //   Navigator.of(context).pop();
+      // }
     }
+    setState(() {
+      _isLoading = false;
+    });
+    Navigator.of(context).pop();
   }
 
   @override
